ade7c087b97d0de18261b8cd76d673ed3d8e95af
daaud <52588294+daaud1@users.noreply.github.com>
2021-03-10 21:06:02 +0000

    Update api.js

--- modules/api.js
+++ modules/api.js
@@ -10,6 +10,7 @@ async function requestAPI (filters, structure) {
     filter_parameter = filter_parameter.slice(0,filter_parameter.length-1); // Removes last semi colon
     let parameter= filter_parameter + '&structure=' + JSON.stringify(structure); // Concatenation of string
     const response = await fetch("https://api.coronavirus.data.gov.uk/v1/data?" + parameter); // API Request
+    
     return response.json(); // Return response, parsed as JSON
 }
 
